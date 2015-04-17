eddf3d5308efcc36314e8d81fdabcc2112d4f44e
fdwillis <fdwillis7@gmail.com>
2015-04-17 12:31:21 -0500

    added a form to create a new post

--- realpython/django-blog/blog_project/blog_app/urls.py
+++ realpython/django-blog/blog_project/blog_app/urls.py
@@ -4,5 +4,6 @@ from blog_app import views
 urlpatterns = patterns(
 	'blog_app.views',
 	url(r'^$', views.index, name = 'index'),
+	url(r'^add_post/', views.add_post, name = "add_post"),
 	url(r"^(?P<post_url>\w+)/$", views.post, name='post'),
 )
--- realpython/django-blog/blog_project/blog_app/views.py
+++ realpython/django-blog/blog_project/blog_app/views.py
@@ -1,24 +1,52 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from blog_app.models import Post
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+from blog_app.forms import PostForm
 
 # Create your views here.
 
+def encode_url(url):
+	return url.replace(" ", "_")
+
+def template(page):
+	return loader.get_template("blog/" + page)
+
 def index(request):
 	latest_posts = Post.objects.all().order_by('-created_at')
-	t = loader.get_template('blog/index.html')
-	context_dict = {'latest_posts': latest_posts, }
+	popular_posts = Post.objects.order_by('-views')[:5]
+	t = template("index.html")
+	context_dict = {
+		'latest_posts': latest_posts,
+		'popular_posts': popular_posts,
+  }
 	for post in latest_posts:
-		post.url = post.title.replace(" ", "_")
+		post.url = encode_url(post.title)
+	for post in popular_posts:
+		post.url = encode_url(post.title)
 	c = Context(context_dict)
 	return HttpResponse(t.render(c))
 
 def post(request, post_url):
 	single_post = get_object_or_404(Post, 
-		title=post_url.replace("_", " "))
+		title = post_url.replace("_", " "))
 	single_post.views += 1
 	single_post.save()
-	t = loader.get_template("blog/post.html")
+	t = template("post.html")
 	c = Context({'single_post': single_post, })
 	return HttpResponse(t.render(c))
+
+
+def add_post(request):
+	context = RequestContext(request)
+	if request.method == 'POST':
+		form = PostForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save(commit = True)
+			return redirect(index)
+		else:
+			print form.errors
+	else:
+		form = PostForm()
+	return render_to_response('blog/add_post.html', {'form': form},
+		context)
